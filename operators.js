687d4f490f716f77137ed8ee4e6b9ccede2cd6b6
danielbranch-dev <daniel.branchdev@gmail.com>
2025-09-26 04:26:35 -0400

    operators.js

--- operators.js
+++ operators.js
@@ -1,4 +1,4 @@
-/*In JavaScript, there are many types of operators. For now focus on Assignment vs Arithmetic operators.*/
+/*In JavaScript, there are many types of operators.*/
 
 /* It is said that JavaScript is a language that determines associactivity from the left, and from the right. Arithmetic operators use math logical opertors like -,+,/,*,**,%.
 When trying to solve a math equation in JS using arithmetic operators, the associativity is read from left to right generally speaking. Also note that operator precedence assigns
@@ -13,3 +13,27 @@ x = 4-5-2;
 x = z=s=r=t=0;
 
 /* In this case, 0 is a value assigned to t. t is assigned to r. r is assigned to s. s is assigned to z. z is then assigned to x. */
+
+
+
+
+
+/*JavaScript operators also have Logical operators. And = &&, OR = ||, NOT = ! */
+
+/* OR Operator */
+
+/* The program below returns the users preferred favourite icecream, Or vanilla is assigned to the favouriteIcecream variable */
+
+let favoriteIcecream = userPreference.favouriteIcecream || "Vanilla";
+
+/*AND operator and NOT operator*/
+
+/*The program below shows the login page when a LoggedInUser is NOT true and displays the showLogInPage variable*/
+const LogInPage = !LoggedInUser && showLogInPage;
+
+/* If Not registered, alert register now */
+if (!isRegistered) { alert ("Register now!"); }
+
+/* True, I like Vanilla IceCream */
+!!"I like Vanilla IceCream"; // Returns true,
+
